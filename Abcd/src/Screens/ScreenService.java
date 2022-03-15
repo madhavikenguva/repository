afd51a39ab019e2d43a36cc8806883f01834f588
madhavikenguva <100989982+madhavikenguva@users.noreply.github.com>
2022-03-15 11:03:53 +0530

    screens
    Virtualkey

---
+++ Abcd/src/Screens/ScreenService.java
@@ -0,0 +1,25 @@
+package Screens;
+
+
+public class ScreenService {
+		
+	public static WelcomeScreen WelcomeScreen = new WelcomeScreen();
+    public static FileOptionScreen FileOptionsScreen = new FileOptionScreen();
+    
+    
+
+    public static Screen CurrentScreen = (Screen) WelcomeScreen;
+
+    
+    public static Screen getCurrentScreen() {
+        return CurrentScreen;
+    }
+
+    
+    public static void setCurrentScreen(Screen currentScreen) {
+        CurrentScreen = currentScreen;
+    }
+    
+    
+    
+}
---
+++ Abcd/src/VirtualKeyApplication.java
@@ -0,0 +1,13 @@
+import Screens.WelcomeScreen;
+
+public class VirtualKeyApplication {
+
+	    public static void main(String[] args) {
+	    	
+	    	WelcomeScreen welcome = new WelcomeScreen();
+	    	welcome.introWS();
+	    	welcome.GetUserInput();
+
+	    }
+	}
+
